b65483cae5e996108359755b2d9fa7468ac4267e
Anthony Xie <xiea@bu.edu>
2025-09-12 20:35:42 -0400

    Rename Django project from mysite to cs412 for proper deployment

--- cs412/settings.py
+++ cs412/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'cs412.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'cs412.wsgi.application'
 
 
 # Database
